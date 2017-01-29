02c2921ff38b8be323768c62212faf6a25cdf20b
chudziutkaa <klaudiachudy94@gmail.com>
2017-01-29 21:04:53 +0100

    Add relationship between boards & lists

--- backend/app/models/list.rb
+++ backend/app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 20, too_long: "%{count} characters is the maximum name's length" }
   has_many :cards
+  belongs_to :board
 end
