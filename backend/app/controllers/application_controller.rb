733816377bcbc059907cdf6beba721699122f104
chudziutkaa <klaudiachudy94@gmail.com>
2017-03-30 22:50:07 +0200

    Authentication helper

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,4 +3,32 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
+  protected
+
+  def authenticate_request!
+    if !payload
+      return invalid_authentication
+    end
+
+    load_current_user!
+    invalid_authentication unless @current_user
+  end
+
+  def invalid_authentication
+    render json: {error: 'Invalid Request'}, status: :unauthorized
+  end
+
+  private
+
+  def payload
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last
+    JsonWebToken.decode(token)
+  rescue
+    nil
+  end
+
+  def load_current_user!
+    @current_user = User.find_by(id: payload['user_id'])
+  end
 end
